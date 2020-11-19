932ee81cc1c54ae4b31531607857485689273b2e
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-11-19 00:45:36 -0500

    feat(songs): made song tracklist

---
+++ modal.js
@@ -0,0 +1,14 @@
+console.log("🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆")
+function showdiv(){
+    document.getElementById("openModal").style.visibility="visible";
+    console.log("SHOWN DIV🔥🔥🔥🔥🔥🔥🔥")
+}
+function closediv(){
+    document.getElementById("openModal").style.visibility="hidden";
+    document.getElementById("openModal").style.display="none";
+    document.getElementById("if").style.visibility="hidden";
+    // document.getElementById("modal-content").style.visibility="hidden";
+    console.log("CLOSED all🔥🔥🔥🔥🔥🔥🔥")
+}
+setTimeout("showdiv()", 1500);
+
