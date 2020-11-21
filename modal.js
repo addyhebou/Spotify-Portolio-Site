f049686f73eaed2ca22147a0756143abc4fc70a2
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-11-21 15:46:49 -0500

    feat(resume): added resume on portfolio

--- modal.js
+++ modal.js
@@ -1,14 +1,25 @@
-console.log("🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆")
-function showdiv(){
-    document.getElementById("openModal").style.visibility="visible";
-    console.log("SHOWN DIV🔥🔥🔥🔥🔥🔥🔥")
-}
-function closediv(){
-    document.getElementById("openModal").style.visibility="hidden";
-    document.getElementById("openModal").style.display="none";
-    document.getElementById("if").style.visibility="hidden";
-    // document.getElementById("modal-content").style.visibility="hidden";
-    console.log("CLOSED all🔥🔥🔥🔥🔥🔥🔥")
-}
-setTimeout("showdiv()", 1500);
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
